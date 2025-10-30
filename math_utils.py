0edbb90378fa6f9f95c7ae565177ed30ef70ab3c
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-10-30 13:35:33 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,8 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 > num2 >= num3:
+      return num1
+    elif num2 > num1 >= num3:
+      return num2
+    if num3 > num2 >= num1:
+      return num3     
+   
